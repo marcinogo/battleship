c9e6c8d357e4d8efc4dcd6113b57170f48968f67
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-04-18 09:13:26 +0200

    Add javadocs

--- src/main/java/pl/wilki/battleship/board/FieldStateChanger.java
+++ src/main/java/pl/wilki/battleship/board/FieldStateChanger.java
@@ -1,9 +1,19 @@
 package pl.wilki.battleship.board;
 
 /**
+ * Responsible for change state of board field to different state
+ * depending on implementation.
+ *
  * @author Marcin Ogorzalek
  */
 interface FieldStateChanger {
 
+  /**
+   * Return new Field object with different state to parameter if change is needed.
+   * Otherwise return Field from parameter
+   *
+   * @param field Field state that can be change if it is needed.
+   * @return new Field with different state or Field form parameter
+   */
   Field changeFieldState(Field field);
 }
