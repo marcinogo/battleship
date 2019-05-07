7afe954684ffbec542008b326d3c4db7a2d52c45
drusil <Mko0,lp!>
2019-05-07 08:39:05 +0200

    initialized turn class structure

---
+++ src/main/java/pl/wilki/battleship/turn/Field.java
@@ -0,0 +1,7 @@
+package pl.wilki.battleship.turn;
+
+/**
+ * @author Jakub Czajka
+ */
+public enum Field {
+}
---
+++ src/main/java/pl/wilki/battleship/turn/Fleet.java
@@ -0,0 +1,7 @@
+package pl.wilki.battleship.turn;
+
+/**
+ * @author Jakub Czajka
+ */
+class Fleet {
+}
