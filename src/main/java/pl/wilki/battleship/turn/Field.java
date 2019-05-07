e1e440ba4e040d9cc3def6091e69bd84b385dc8e
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-05-07 09:23:27 +0200

    Create enum Field with overriden toString()
    Due to changes in an application architecture new version of is
    implemented. It is now simple enum with information about color
    representation of given field.

--- src/main/java/pl/wilki/battleship/turn/Field.java
+++ src/main/java/pl/wilki/battleship/turn/Field.java
@@ -1,7 +1,22 @@
 package pl.wilki.battleship.turn;
 
 /**
- * @author Jakub Czajka
+ * @author Marcin Ogorzalek
  */
 public enum Field {
+  WATER("WHITE"),
+  WATER_SHOT("BLUE"),
+  SHIP("RED"),
+  SHIP_SHOT("BLACK");
+
+  private String color;
+
+  Field(String color) {
+    this.color = color;
+  }
+
+  @Override
+  public String toString() {
+    return color;
+  }
 }
